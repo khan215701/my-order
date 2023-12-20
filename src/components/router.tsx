23acfca61a0ba7e2ed8259e88b84c10864b10ecc
khan safiq <>
2023-12-20 22:45:59 +0530

    register page with material ui

--- src/components/router.tsx
+++ src/components/router.tsx
@@ -1,6 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import AuthRegister from "./AuthRegister";
 
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+const router = createBrowserRouter([
+  /* home page */
+  { path: "/", element: <HomePage /> },
+
+  /* auth pages*/
+  { path: "/auth-register", element: <AuthRegister /> },
+]);
 
 export default router;
